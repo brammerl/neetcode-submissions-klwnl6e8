bd291030f8b24f89aeecba50ef9c3bde040b9b07
Lucia Brammer <brammerl96@gmail.com>
2026-04-08 13:46:57 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap_1, hashmap_2 = {}, {}
+        s1, s2 = list(s), list(t)
+
+        print(s1)
+        print(s2)
+
+        for s in s1:
+            value = hashmap_1.get(s)
+            if value != None:
+                hashmap_1[s] = hashmap_1[s] + 1
+            else:
+                hashmap_1[s] = 1
+        for t in s2:
+            value = hashmap_2.get(t)
+           
+            if value != None:
+                hashmap_2[t] = hashmap_2[t] + 1
+            else: 
+                hashmap_2[t] = 1
+        
+        return hashmap_2 == hashmap_1
+            
+
+
+        
