92d5d58c4633b75ef727b94fe43b31e1da039621
Lucia Brammer <brammerl96@gmail.com>
2026-04-17 14:03:34 -0700

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        minimum = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(minimum)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        print
+        print(self.minStack)
+        return self.minStack[-1]
